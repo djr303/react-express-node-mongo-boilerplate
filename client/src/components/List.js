2f42f317a8f698a11a209e869bb601df02e5f8ac
David Roberts <davidroberts@David-Roberts-MacBook-Pro.local>
2021-11-18 21:09:53 +0000

    Stopped work due to copyright conerns

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -5,49 +5,55 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(id, publisher, title, url, tags, date) {
+  return { id, publisher, title, url, tags, date };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(28, 'the guardian', 'It beggers belief', 'www.example.com', 'jesus, politics', '11/08/21'),
+  createData(19, 'the times', 'boris gets it the back', 'www.example.com', 'tory, westminster, politics', '12/09/21'),
+  createData(15, 'the sun', 'football crazy', 'www.example.com', 'sport, politics, money', '14/09/21'),
+  createData(70, 'medium', 'hello world version 500', 'www.example.com', 'tutorials, python, tech', '13/09/20'),
 ];
 
-const styles = {
+const rowStyles = {
   color: "#777",
   borderBottom: "1px dotted #777"
 }
 
+const headerStyles = { ...rowStyles, fontWeight: 'bold'}
+
 const List = () => (
   <Fragment>
     <TableContainer>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ width: '100%' }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell sx={styles}>Dessert (100g serving)</TableCell>
-            <TableCell sx={styles} align="right">Calories</TableCell>
-            <TableCell sx={styles} align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell sx={styles} align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell sx={styles} align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell sx={headerStyles} align="left">publisher</TableCell>
+            <TableCell sx={headerStyles} align="left">article title</TableCell>
+            <TableCell sx={headerStyles} align="left">url</TableCell>
+            <TableCell sx={headerStyles} align="left">tags</TableCell>
+            <TableCell sx={headerStyles} align="left">date</TableCell>
+            <TableCell sx={headerStyles} align="left">edit/view</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.id}
             >
-              <TableCell sx={styles} scope="row">
-                {row.name}
+              <TableCell sx={rowStyles} scope="row">
+                {row.publisher} 
               </TableCell>
-              <TableCell sx={styles} align="right">{row.calories}</TableCell>
-              <TableCell sx={styles} align="right">{row.fat}</TableCell>
-              <TableCell sx={styles} align="right">{row.carbs}</TableCell>
-              <TableCell sx={styles} align="right">{row.protein}</TableCell>
+              <TableCell sx={rowStyles} align="left">{row.title}</TableCell>
+              <TableCell sx={rowStyles} align="left"><a href={row.url}>{row.url}</a></TableCell>
+              <TableCell sx={rowStyles} align="left">{row.tags}</TableCell>
+              <TableCell sx={rowStyles} align="left">{row.date}</TableCell>
+              <TableCell sx={rowStyles} align="left">
+                <Button href={`/edit:${row.id}`}>Edit/View</Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
